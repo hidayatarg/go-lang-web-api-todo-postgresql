4aedb9ccb08685c997e80520cb3768aac0de32ec
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-02-22 16:45:03 +0300

    Setup the First Middleware

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,23 @@
 package handler
 
-import "github.com/go-chi/chi"
+import (
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 type Server struct {
+}
+
+func setupMiddlewares(r *chi.Mux) {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Compress(6, "application/json"))
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(middleware.Timeout(60 * time.Second))
 }
 
 // Constructor
@@ -16,6 +31,8 @@ func SetupRouter() *chi.Mux {
 
 	// Get HTTP Req
 	r := chi.NewRouter()
+
+	setupMiddlewares(r)
 
 	server.setupEndpoints(r)
 
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ To Create a Go Lang Package `go mod init todo`. (in JS like npm init).
 
 ### Note
 Import but not used `_ "github.com/lib/pq"`
+
+### Middleware
+go to `http://localhost:8081/api/v1/users`
